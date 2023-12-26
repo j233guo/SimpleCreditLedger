47e79a010da238982798f35de3914022f15b7219
Jiaming Guo <64711081+j233guo@users.noreply.github.com>
2023-12-25 19:41:50 -0800

    Initial Commit

---
+++ SimpleCreditLedger/SimpleCreditLedgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleCreditLedgerApp.swift
+//  SimpleCreditLedger
+//
+//  Created by Jiaming Guo on 2023-12-25.
+//
+
+import SwiftUI
+
+@main
+struct SimpleCreditLedgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
